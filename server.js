aa58eb3bde4851f5e0a12212fd8f79a6cd32fb35
moneymind <30737859+moneymind@users.noreply.github.com>
2017-08-10 04:58:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (rendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
