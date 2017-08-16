a94e1c795a73b5b57cbe98ba701c71a617ecb97f
moneymind <30737859+moneymind@users.noreply.github.com>
2017-08-16 19:36:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,15 +30,11 @@ app.get('/ui/madi.png',function(req,res) {
 });
 
 
-var port-8080;//use 8080 for local development because you might already have apache running on 80
+var port=8080;//use 8080 for local development because you might already have apache running on 80
 app.listen(8080,function(){
-    console.log ('IMAD  course app  listening on port' $ {port}!'');
+    console.log ('IMAD  course app  listening on port $ {port}!');
 });
 
 
 
 
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
